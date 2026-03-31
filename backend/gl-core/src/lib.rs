2c0a2895ee67496f06e504821115dac2ae070716
James Tien <southp0105@gmail.com>
2026-03-31 12:48:06 +0800

    Implemented a simple `new` function for Goopy

--- backend/gl-core/src/lib.rs
+++ backend/gl-core/src/lib.rs
@@ -1,16 +1,38 @@
 use chrono::{DateTime, Utc};
 
+#[derive(Debug)]
+pub enum GlError {
+    Failed(String),
+    Invalid,
+    NotFound,
+}
+
 pub struct Goopy {
     pub slug: String,
     pub life_in_days: i32,
     pub created_at: DateTime<Utc>,
 }
 
-#[derive(Debug)]
-pub enum GlError {
-    Failed(String),
-    Invalid,
-    NotFound,
+impl Goopy {
+    pub fn new(
+        slug: String,
+        life_in_days: i32,
+        created_at: DateTime<Utc>,
+    ) -> Result<Self, GlError> {
+        if life_in_days <= 0 {
+            return Err(GlError::Invalid);
+        }
+
+        if slug.is_empty() {
+            return Err(GlError::Invalid);
+        }
+
+        Ok(Self {
+            slug,
+            life_in_days,
+            created_at,
+        })
+    }
 }
 
 pub trait GoopyRuntime {
