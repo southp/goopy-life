6d86d98b33823303b7b38d0248b5456ead11f187
James Tien <southp0105@gmail.com>
2026-04-23 16:27:55 +0800

    Draft `GoopyStore` trait

--- backend/gl-core/src/shared_types.rs
+++ backend/gl-core/src/shared_types.rs
@@ -1,3 +1,5 @@
+use crate::goopy::Goopy;
+
 #[derive(Debug)]
 pub enum GlError {
     Failed(String),
@@ -13,3 +15,17 @@ pub enum GlStatus {
     Done,
 }
 
+#[derive(Debug)]
+pub enum StoreError {
+    NotFound,
+    AlreadyExists,
+    Io(std::io::Error),
+    Other(String),
+}
+
+pub trait GoopyStore {
+    fn save(&self, gp: &Goopy) -> Result<(), StoreError>;
+    fn load(&self, slug: &String) -> Result<Option<Goopy>, StoreError>;
+    fn delete(&self, slug: &String) -> Result<(), StoreError>;
+    fn list(&self) -> Result<Vec<Goopy>, StoreError>;
+}
