09ab37044a7a5e21ad60465230d8101bdbd6e47b
James Tien <southp0105@gmail.com>
2026-05-14 16:27:11 +0800

    Scafold the page structure

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,62 @@
+"use client";
+
+import { useState } from 'react';
+
+type Status = { kind: 'idle' }
+	| { kind: 'busy' }
+	| { kind: 'done'; url: string }
+;
+
+function GoButton({ status, onClick }: {status: Status, onClick: () => void }) {
+	if (status.kind === 'idle') {
+		return (
+			<button className="ghost-button idle" onClick={onClick}>
+				Ghost now!
+			</button>
+		);
+	}
+
+	if (status.kind === 'busy') {
+		return (
+			<p> Ohhhh ... I am busy ... </p>
+		);
+	}
+
+	if (status.kind === 'done') {
+		return (
+			<>
+				<p> Your Ghost instance is ready at: <br/>
+					<a href={status.url} >
+						https://foo.goopy.life
+					</a>
+				</p>
+				<p> Redirect you in 5 ...</p>
+			</>
+		)
+	}
+
+}
+
 export default function Home() {
-  return (
-    <div className="page-wrapper">
-      <main className="page-main">
-        <span className="hero-emoji">💩</span>
-        <a
-          className="ghost-button"
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ghost now!
-        </a>
-      </main>
-    </div>
-  );
+	const [status, setStatus] = useState<Status>({ kind: 'idle' });
+
+	const onClick = () => {
+		setStatus({kind: "busy"});
+		setTimeout(() => {
+			setStatus( {
+				kind: "done",
+				url: "https://foo.southp.dev",
+
+			} );
+		}, 3000);
+	}
+
+	return (
+		<div className="page-wrapper">
+			<main className="page-main">
+				<span className="hero-emoji">💩</span>
+				<GoButton status={ status } onClick={ onClick } />
+			</main>
+		</div>
+	);
 }
